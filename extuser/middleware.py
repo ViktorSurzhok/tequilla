0d2365ac215266cf09963bd7fb79b6102125c028
lionasp <lion.programmer@gmail.com>
2016-06-03 18:06:25 +0400

    fix user logout

--- extuser/middleware.py
+++ extuser/middleware.py
@@ -5,11 +5,12 @@ from extuser.models import UserActivityLog
 
 class UserActivityLogMiddleware(object):
     def process_request(self, request):
-        date = datetime.date.today()
-        try:
-            log = UserActivityLog.objects.get(created=str(date), user=request.user)
-            log.save(force_update=True)
-        except UserActivityLog.DoesNotExist:
-            UserActivityLog.objects.create(created=str(date), user=request.user)
+        if request.user.is_authenticated():
+            date = datetime.date.today()
+            try:
+                log = UserActivityLog.objects.get(created=str(date), user=request.user)
+                log.save(force_update=True)
+            except UserActivityLog.DoesNotExist:
+                UserActivityLog.objects.create(created=str(date), user=request.user)
 
         return None
